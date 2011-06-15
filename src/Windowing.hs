5c3364d75ce8fbecd787f2d404ee5704ccd1e7f3
Dave Tapley <dave.a.tapley@gmail.com>
2011-06-15 11:18:39 +0100

    better naming for ref

--- src/Windowing.hs
+++ src/Windowing.hs
@@ -7,27 +7,27 @@
     newWindow :: IO () -> IO ()
     newWindow renderWorldContent = do
         (progName, args) <- getArgsAndInitialize
-        env <- newIORef (Vertex3 0 0 (-100))
+        lookAtRef <- newIORef (Vertex3 0 0 (-100))
         createWindow progName
-        displayCallback $= display renderWorldContent env
-        keyboardMouseCallback $= Just (kbm renderWorldContent env)
+        displayCallback $= display renderWorldContent lookAtRef
+        keyboardMouseCallback $= Just (kbm renderWorldContent lookAtRef)
         actionOnWindowClose $= MainLoopReturns
         mainLoop
 
     display :: IO () -> IORef (Vertex3 GLdouble) -> DisplayCallback
-    display renderWorldContent env = do
+    display renderWorldContent lookAtRef = do
         clear [ColorBuffer]
-        pov <- readIORef env
+        pov <- readIORef lookAtRef
         loadIdentity
         lookAt (Vertex3 0 0 0) pov (Vector3 0 1 0)
         renderWorldContent
         flush
 
     kbm :: IO () -> IORef (Vertex3 GLdouble) -> KeyboardMouseCallback
-    kbm renderWorldContent env key keystate modifiers position = do
+    kbm renderWorldContent lookAtRef key keystate modifiers position = do
          case key of
-             SpecialKey KeyLeft      -> do {modifyIORef env (\(Vertex3 x y z) -> (Vertex3 (x-1) y  z)); display renderWorldContent env }
-             SpecialKey KeyRight     -> do {modifyIORef env (\(Vertex3 x y z) -> (Vertex3 (x+1) y  z)); display renderWorldContent env }
-             SpecialKey KeyUp        -> do {modifyIORef env (\(Vertex3 x y z) -> (Vertex3 x (y-1)  z)); display renderWorldContent env }
-             SpecialKey KeyDown      -> do {modifyIORef env (\(Vertex3 x y z) -> (Vertex3 x (y+1)  z)); display renderWorldContent env }
+             SpecialKey KeyLeft      -> do {modifyIORef lookAtRef (\(Vertex3 x y z) -> (Vertex3 (x-1) y  z)); display renderWorldContent lookAtRef }
+             SpecialKey KeyRight     -> do {modifyIORef lookAtRef (\(Vertex3 x y z) -> (Vertex3 (x+1) y  z)); display renderWorldContent lookAtRef }
+             SpecialKey KeyUp        -> do {modifyIORef lookAtRef (\(Vertex3 x y z) -> (Vertex3 x (y-1)  z)); display renderWorldContent lookAtRef }
+             SpecialKey KeyDown      -> do {modifyIORef lookAtRef (\(Vertex3 x y z) -> (Vertex3 x (y+1)  z)); display renderWorldContent lookAtRef }
              _                       -> return ()
